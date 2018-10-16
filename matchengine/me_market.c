b26f03b91642eb694bffc6d94b724631a1f11c22
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-16 12:51:00 -0400

    stoploss: Verify balance

--- matchengine/me_market.c
+++ matchengine/me_market.c
@@ -570,10 +570,15 @@ static int execute_limit_bid_order(bool real, market_t *m, order_t *taker)
 int market_put_stop_loss_order(bool real, json_t **result, market_t *m, uint32_t user_id, uint32_t side, mpd_t *trigger, mpd_t *amount, mpd_t *taker_fee, const char *source)
 {
     if (side == MARKET_ORDER_SIDE_ASK) {
-        return -100; //TEMP
+        mpd_t *balance = balance_get(user_id, BALANCE_TYPE_AVAILABLE, m->stock);
+        if (!balance || mpd_cmp(balance, amount, &mpd_ctx) < 0) {
+            return -1;
+        }
     } else {
         return -101;
     }
+    
+    return -100; //TEMP
 }
 
 int market_put_limit_order(bool real, json_t **result, market_t *m, uint32_t user_id, uint32_t side, mpd_t *amount, mpd_t *price, mpd_t *taker_fee, mpd_t *maker_fee, const char *source)
