9ab96bd2ff5b79b35ba15d0fdbe8ce238f047d41
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-22 13:46:04 -0400

    stoploss: Add buy stop orders to stop_bids list

--- matchengine/me_market.c
+++ matchengine/me_market.c
@@ -198,10 +198,13 @@ static int order_put(market_t *m, order_t *order)
         if (balance_freeze(order->user_id, m->stock, order->left) == NULL)
             return -__LINE__;
     } else {
-        if (order->type == MARKET_ORDER_TYPE_STOP_LOSS)
-            return -__LINE__;
-        if (skiplist_insert(m->bids, order) == NULL)
-            return -__LINE__;
+        if (order->type == MARKET_ORDER_TYPE_STOP_LOSS) {
+            if (skiplist_insert(m->stop_bids, order) == NULL)
+                return -__LINE__;
+        } else {
+            if (skiplist_insert(m->bids, order) == NULL)
+                return -__LINE__;
+        }
         mpd_t *result = mpd_new(&mpd_ctx);
         mpd_mul(result, order->price, order->left, &mpd_ctx);
         mpd_copy(order->freeze, result, &mpd_ctx);
