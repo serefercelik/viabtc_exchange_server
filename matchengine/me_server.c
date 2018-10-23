d4742da8ec806f684fd186553ba345ed9714497f
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-22 21:12:33 -0400

    stoplimit: Extract param source

--- matchengine/me_server.c
+++ matchengine/me_server.c
@@ -510,6 +510,13 @@ static int on_cmd_order_put_stop_limit(nw_ses *ses, rpc_pkg *pkg, json_t *params
     if (maker_fee == NULL || mpd_cmp(maker_fee, mpd_zero, &mpd_ctx) < 0 || mpd_cmp(maker_fee, mpd_one, &mpd_ctx) >= 0)
         goto invalid_argument;
     
+    // source
+    if (!json_is_string(json_array_get(params, 8)))
+        goto invalid_argument;
+    const char *source = json_string_value(json_array_get(params, 8));
+    if (strlen(source) >= SOURCE_MAX_LEN)
+        goto invalid_argument;
+    
     mpd_del(trigger);
     mpd_del(amount);
     mpd_del(price);
