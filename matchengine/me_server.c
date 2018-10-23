a3cec95d9af906a2343606a65fef25406d761626
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-22 21:02:44 -0400

    stoplimit: Extract param amount

--- matchengine/me_server.c
+++ matchengine/me_server.c
@@ -470,6 +470,7 @@ static int on_cmd_order_put_stop_limit(nw_ses *ses, rpc_pkg *pkg, json_t *params
         return reply_error_invalid_argument(ses, pkg);
     
     mpd_t *trigger      = NULL;
+    mpd_t *amount       = NULL;
     
     // trigger
     if (!json_is_string(json_array_get(params, 3)))
@@ -478,13 +479,23 @@ static int on_cmd_order_put_stop_limit(nw_ses *ses, rpc_pkg *pkg, json_t *params
     if (trigger == NULL || mpd_cmp(trigger, mpd_zero, &mpd_ctx) <= 0)
         goto invalid_argument;
     
+    // amount
+    if (!json_is_string(json_array_get(params, 4)))
+        goto invalid_argument;
+    amount = decimal(json_string_value(json_array_get(params, 4)), market->stock_prec);
+    if (amount == NULL || mpd_cmp(amount, mpd_zero, &mpd_ctx) <= 0)
+        goto invalid_argument;
+    
     mpd_del(trigger);
+    mpd_del(amount);
     
     return reply_error(ses, pkg, -101, "put stop limit not implemented");
 
 invalid_argument:
     if (trigger)
         mpd_del(trigger);
+    if (amount)
+        mpd_del(amount);
     
     return reply_error(ses, pkg, -101, "put stop limit not implemented");
 }
